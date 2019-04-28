d26ca88efea4df95da106516ce95edf24ac45e1e
ShaBirgen <sharoncherotich112@gmail.com>
2019-04-28 15:46:53 +0300

    changed child to student

---
+++ justkidding.py
@@ -0,0 +1,29 @@
+# my first python
+
+name = (input("Enter name: "))
+
+print(name)
+
+FOOD = input("enter my favourite food: ")
+food = FOOD.lower()
+
+if food == "chicken":
+    print(name + " Tunaenda Chicken inn lini?!")
+else:
+    print(name + " whatever si you are boring hta")
+
+DRINK = input(" enter my favourite drink: ")
+drink = DRINK.lower()
+
+if drink == "water":
+    print(name + " Water is life ")
+else:
+    print("aki " + name + " Who doesn`t love water")
+
+SHOES = input("enter my worse type of shoes: ")
+
+shoes = SHOES.lower()
+if shoes == "heels":
+    print("wow " + name + " join me in hating them")
+else:
+    print(name + "sasa unanipeleka house of leather lini")
